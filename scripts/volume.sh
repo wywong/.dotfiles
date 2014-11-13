c154341dd87856de818931c4b818ccdf1af1419b
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2014-11-12 22:14:07 -0800

    volume.sh script now only outputs the first match

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
 printf "Volume: "
-# amixer get 'Master' | egrep -0o "\[[0-9]+%\]|\[on\]|\[off\]" | tr "\\n" " "
-amixer get 'Master' | egrep -0o "[0-9]+%|\[on\]|\[off\]" | tr "\\n" " "
+amixer get 'Master' | egrep -0o -m 1 "[0-9]+%|\[on\]|\[off\]" | tr "\\n" " "
 
